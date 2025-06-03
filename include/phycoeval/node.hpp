1a9b840d47c4a4b8621f9034d21310edcdd6cd1e
Jamie R. Oaks <joaks1@gmail.com>
2025-06-03 11:34:38 -0500

    Initial commit of node and tree classes.

---
+++ include/phycoeval/node.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <iostream>
+//#include "split.hpp"
+
+namespace phycoeval {
+ 
+    //class Tree;
+    //class TreeManip;
+    //class Likelihood;
+    //class Updater;
+
+    class Node {
+        //friend class Tree;
+        //friend class TreeManip;
+        //friend class Likelihood;
+        //friend class Updater;
+
+        public:
+                                    Node();
+                                    ~Node();
+
+            Node *                  get_parent()        {return this->_parent;}
+            Node *                  get_left_child()    {return this->_left_child;}
+            Node *                  get_right_sib()     {return this->_right_sib;}
+            int                     get_number() const  {return this->_number;}
+            std::string             get_name() const    {return this->_name;}
+            //Split                   get_spit() const    {return this->_split;}
+            //
+            double                  get_height() const  {return this->_height;}
+            void                    set_height(double height);
+
+            bool                    has_parent() const;
+            bool                    has_child() const;
+            bool                    is_root() const;
+            bool                    is_leaf() const;
+            double                  get_edge_length() const;
+
+            static const double _smallest_edge_length;
+
+            typedef std::vector<Node>       Vector;
+            typedef std::vector<Node *>     PtrVector;
+
+        private:
+ 
+            void                clear();
+            Node *              _left_child;
+            Node *              _right_sib;
+            Node *              _parent;
+            int                 _number;
+            std::string         _name;
+            double              _height;
+            //Split               _split;
+    };
+
+    inline Node::Node() {
+        std::cout << "Creating Node object" << std::endl;
+        this->clear();
+    }
+
+    inline Node::~Node() {
+        std::cout << "Destroying Node object" << std::endl;
+    }
+
+    inline void Node::clear() {
+        _left_child = 0;
+        _right_sib = 0;
+        _parent = 0;
+        _number = -1;
+        _name = "";
+        _height = 0.0;
+    }
+
+    inline void Node::set_height(double height) {
+        this->_height = height;
+    }
+
+    inline bool Node::has_parent() const {
+        return this->_parent == 0 ? false : true;
+    }
+
+    inline bool Node::has_child() const {
+        return this->_left_child == 0 ? false : true;
+    }
+
+    inline bool Node::is_root() const {
+        return this->_parent == 0 ? true : false;
+    }
+
+    inline bool Node::is_leaf() const {
+        return this->_left_child == 0 ? true : false;
+    }
+
+    inline double Node::get_edge_length() const {
+        if (this->has_parent()) {
+            return this->_parent->get_height() - this->get_height();
+        }
+        return 0.0;
+    }
+}
---
+++ include/phycoeval/tree.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <memory>
+#include <iostream>
+#include "node.hpp"
+
+namespace phycoeval {
+
+    //class TreeManip;
+    //class Likelihood;
+    //class Updater;
+
+    class Tree {
+        //friend class TreeManip;
+        //friend class Likelihood;
+        //friend class Updater;
+
+        public:
+
+                                        Tree();
+                                        ~Tree();
+
+            unsigned                    num_leaves() const;
+            unsigned                    num_internals() const;
+            unsigned                    num_nodes() const;
+
+        private:
+
+            void                        clear();
+
+            Node *                      _root;
+            unsigned                    _nleaves;
+            unsigned                    _ninternals;
+            Node::PtrVector             _preorder;
+            Node::PtrVector             _levelorder;
+            Node::Vector                _nodes;
+
+        public:
+
+            typedef std::shared_ptr< Tree > SharedPtr;
+    };
+
+    inline Tree::Tree() {
+        std::cout << "Constructing a Tree" << std::endl;
+        this->clear();
+    }
+
+    inline Tree::~Tree() {
+        std::cout << "Destroying a Tree" << std::endl;
+    }
+
+    inline void Tree::clear() {
+        this->_root = 0;
+        this->_nodes.clear();
+        this->_preorder.clear();
+        this->_levelorder.clear();
+    }
+
+    inline unsigned Tree::num_leaves() const {
+        return _nleaves;
+    }
+
+    inline unsigned Tree::num_internals() const {
+        return _ninternals;
+    }
+
+    inline unsigned Tree::num_nodes() const {
+        return (unsigned)this->_nodes.size();
+    }
+}
